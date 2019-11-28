49310c1a8d967faa88c5e6b5ba8bb7fcda8e3263
tosaka07 <tosaka.tech@gmail.com>
2019-11-28 14:40:30 +0900

    Initial Commit

---
+++ LearningSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  LearningSwiftUI
+//
+//  Created by 坂上 翔悟 on 2019/11/28.
+//  Copyright © 2019 tosaka.net. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
